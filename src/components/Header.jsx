050fbfa73966c943a9d3d87d9466cc1891ac3d98
Hernán Calvo <hernan.calvo@chediack.com.ar>
2022-12-04 22:39:59 -0300

    Primer Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <h1 className='text-4xl font-extrabold text-gray-500 text-center'>
+        ¿Cuánto <span className='text-indigo-600'>Dinero</span> Necesitas?
+    </h1>
+  )
+}
+
+export default Header
